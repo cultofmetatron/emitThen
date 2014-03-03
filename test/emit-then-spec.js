d34a86743c53030ed18c3edc2d169d5d380e52ac
Peter de Croos <cultofmetatron@Peters-MacBook-Pro.local>
2014-03-02 16:56:46 -0800

    added test spec

---
+++ test/emit-then-spec.js
@@ -0,0 +1,15 @@
+var mocha  = require('mocha');
+var should = require('should');
+
+define('emitThen', function() {
+
+  it('should run the function when emitThen is called', function() {
+  
+  
+  });
+
+
+
+
+
+});
